64f7b1c05de57ebfc6252966b697529f43bf270a
Krzysztof-Kobylkiewicz <krzysztofkobylkiewicz@gmail.com>
2025-05-29 17:30:21 +0200

    Generic repository

--- Core/Repositories/IRepository.cs
+++ Core/Repositories/IRepository.cs
@@ -16,6 +16,29 @@ namespace Core.Repositories
         Task<E[]> GetEntitiesAsync<E, Key>(Func<DbSet<E>, IQueryable<E>> func) where E : Entity<Key> where Key : IEquatable<Key>;
         E? GetEntity<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
         Task<E?> GetEntityAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
+        bool IsEntityAdded<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
+        Task<bool> IsEntityAddedAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
+
+        #endregion
+
+        #region EntityAdding
+
+        E? AddEntity<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>;
+        Task<E?> AddEntityAsync<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>;
+
+        #endregion
+
+        #region EntityUpdating
+
+        E? UpdateEntity<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>;
+        Task<E?> UpdateEntityAsync<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>;
+
+        #endregion
+
+        #region EntityDeleting
+
+        bool DeleteEntity<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
+        Task<bool> DeleteEntityAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>;
 
         #endregion
     }
--- Core/Repositories/Repository.cs
+++ Core/Repositories/Repository.cs
@@ -5,7 +5,7 @@ namespace Core.Repositories
 {
     public abstract class Repository<C> : IRepository<C> where C : DbContext
     {
-        protected C _dbContext;
+        protected readonly C _dbContext;
 
         protected Repository(C dbContext)
         {
@@ -29,6 +29,90 @@ namespace Core.Repositories
         public E? GetEntity<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key> => GetEntityQuery<E, Key>(entity => entity.Where(e => e.Id.Equals(id))).SingleOrDefault();
 
         public async Task<E?> GetEntityAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key> => await GetEntityQuery<E, Key>(entity => entity.Where(e => e.Id.Equals(id))).SingleOrDefaultAsync();
+
+        public bool IsEntityAdded<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key> => GetEntityQuery<E, Key>(entity => entity.Where(e => e.Id.Equals(id))).Any();
+
+        public async Task<bool> IsEntityAddedAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key> => await GetEntityQuery<E, Key>(entity => entity.Where(e => e.Id.Equals(id))).AnyAsync();
+
+        #endregion
+
+        #region EntityAdding
+
+        public E? AddEntity<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            if (IsEntityAdded<E, Key>(entity.Id))
+                throw new InvalidOperationException("Entity is already added.");
+
+            Add<E, Key>(entity);
+            SaveChanges();
+            return entity;
+        }
+
+        private void Add<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key> => _dbContext.Set<E>().Add(entity);
+
+        public async Task<E?> AddEntityAsync<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            if (await IsEntityAddedAsync<E, Key>(entity.Id))
+                throw new InvalidOperationException("Entity is already added.");
+
+            await AddAsync<E, Key>(entity);
+            await SaveChangesAsync();
+            return entity;
+        }
+
+        private async Task AddAsync<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key> => await _dbContext.Set<E>().AddAsync(entity);
+
+        #endregion
+
+        #region EntityUpdating
+
+        public E? UpdateEntity<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            E retrievedEntity = GetEntity<E, Key>(entity.Id) ?? throw new InvalidOperationException("There is no entity to update.");
+
+            _dbContext.Entry(retrievedEntity).CurrentValues.SetValues(entity);
+
+            SaveChanges();
+
+            return retrievedEntity;
+        }
+
+        public async Task<E?> UpdateEntityAsync<E, Key>(E entity) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            E retrievedEntity = await GetEntityAsync<E, Key>(entity.Id) ?? throw new InvalidOperationException("There is no entity to update.");
+
+            _dbContext.Entry(retrievedEntity).CurrentValues.SetValues(entity);
+
+            await SaveChangesAsync();
+
+            return retrievedEntity;
+        }
+
+        #endregion
+
+        #region EntityDeleting
+
+        public bool DeleteEntity<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            var retrievedEntity = GetEntity<E, Key>(id) ?? throw new InvalidOperationException("There is no entity to delete.");
+
+            _dbContext.Remove(retrievedEntity);
+
+            SaveChanges();
+
+            return true;
+        }
+
+        public async Task<bool> DeleteEntityAsync<E, Key>(Key id) where E : Entity<Key> where Key : IEquatable<Key>
+        {
+            var retrievedEntity = await GetEntityAsync<E, Key>(id) ?? throw new InvalidOperationException("There is no entity to delete.");
+
+            _dbContext.Remove(retrievedEntity);
+
+            await SaveChangesAsync();
+
+            return true;
+        }
 
         #endregion
     }
